9e68acabf88b69c15857aeedcc1acb7f08444a52
elyesseadda <elyesseadda@hotmail.com>
2025-09-27 12:02:45 +0200

    Add Emetteur Admin

--- Desktop/Projet/P3000/Application/api/admin.py
+++ Desktop/Projet/P3000/Application/api/admin.py
@@ -11,3 +11,33 @@ class FournisseurMagasinAdmin(admin.ModelAdmin):
     ordering = ('-derniere_utilisation',)
 
 
+@admin.register(Emetteur)
+class EmetteurAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email', 'phone_Number', 'is_active', 'date_creation')
+    list_filter = ('is_active', 'date_creation')
+    search_fields = ('name', 'surname', 'email', 'phone_Number')
+    list_editable = ('phone_Number', 'is_active')
+    ordering = ('surname', 'name')
+    
+    fieldsets = (
+        ('Informations personnelles', {
+            'fields': ('name', 'surname', 'email', 'phone_Number')
+        }),
+        ('Statut', {
+            'fields': ('is_active',),
+            'classes': ('collapse',)
+        }),
+        ('Métadonnées', {
+            'fields': ('date_creation',),
+            'classes': ('collapse',),
+            'description': 'Date de création automatique'
+        }),
+    )
+    
+    readonly_fields = ('date_creation',)
+    
+    def get_queryset(self, request):
+        """Optimise les requêtes pour l'admin"""
+        return super().get_queryset(request).select_related()
+
+
